37d74647c5287834f3eab9ce0d274478a7f3615d
Huy Hoang Le <hhoangle2323@gmail.com>
2023-05-26 21:46:48 +0700

    complete weather search & weather

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoAPIOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '2532a88217mshb6a5952671cfdb5p13a2afjsnbe0c8014ce2f',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+    },
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = 'b594eb07401cacacae76377740516410'
